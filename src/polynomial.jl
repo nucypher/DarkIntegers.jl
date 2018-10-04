5fbdc70edf18aa235b37d8c0c401ce7cf2afa83c
Bogdan Opanchuk <bogdan@opanchuk.net>
2018-10-04 12:41:36 +1000

    polynomial: remove "2" postfixes in Karatsuba functions

--- src/polynomial.jl
+++ src/polynomial.jl
@@ -156,7 +156,7 @@ end
 end
 
 
-@inline function mul_with_overflow2(l, res, res_s, p1, p1_s, p2, p2_s)
+@inline function mul_with_overflow(l, res, res_s, p1, p1_s, p2, p2_s)
     @simd for j in 1:l
         for k in 1:l
             res[res_s+j+k-2] += p2[p2_s+k-1] * p1[p1_s+j-1]
@@ -165,19 +165,19 @@ end
 end
 
 
-@inline function _karatsuba_mul2(full_len, res, res_s, p1c, p1_s, p2c, p2_s, buf, buf_s,
+@inline function _karatsuba_mul(full_len, res, res_s, p1c, p1_s, p2c, p2_s, buf, buf_s,
         buf2, buf2_s)
 
     if full_len <= 8
-        mul_with_overflow2(full_len, res, res_s, p1c, p1_s, p2c, p2_s)
+        mul_with_overflow(full_len, res, res_s, p1c, p1_s, p2c, p2_s)
         return
     end
 
     half_len = div(full_len, 2)
 
-    _karatsuba_mul2(
+    _karatsuba_mul(
         half_len, res, res_s, p1c, p1_s, p2c, p2_s, buf, buf_s+full_len, buf2, buf2_s)
-    _karatsuba_mul2(
+    _karatsuba_mul(
         half_len, res, res_s+full_len, p1c, p1_s+half_len, p2c, p2_s+half_len, buf, buf_s+full_len,
         buf2, buf2_s)
 
@@ -191,7 +191,7 @@ end
         buf2[buf2_s+half_len+i-1] = p2c[p2_s+i-1] + p2c[p2_s+half_len+i-1]
     end
 
-    _karatsuba_mul2(half_len, buf, buf_s, buf2, buf2_s, buf2, buf2_s+half_len, buf, buf_s+full_len,
+    _karatsuba_mul(half_len, buf, buf_s, buf2, buf2_s, buf2, buf2_s+half_len, buf, buf_s+full_len,
         buf2, buf2_s+full_len)
 
     @simd for i in 1:full_len
@@ -223,14 +223,14 @@ end
         r2[i] = z
     end
 
-    _karatsuba_mul2(half_len, r0, 1, p1.coeffs, 1, p2.coeffs, 1, buf, 1, buf2, 1)
-    _karatsuba_mul2(half_len, r2, 1, p1.coeffs, half_len+1, p2.coeffs, half_len+1, buf, 1, buf2, 1)
+    _karatsuba_mul(half_len, r0, 1, p1.coeffs, 1, p2.coeffs, 1, buf, 1, buf2, 1)
+    _karatsuba_mul(half_len, r2, 1, p1.coeffs, half_len+1, p2.coeffs, half_len+1, buf, 1, buf2, 1)
 
     @simd for i in 1:half_len
         r3[i] = p1.coeffs[i] + p1.coeffs[i+half_len]
         r3[i+half_len] = p2.coeffs[i] + p2.coeffs[i+half_len]
     end
-    _karatsuba_mul2(half_len, r1, 1, r3, 1, r3, half_len+1, buf, 1, buf2, 1)
+    _karatsuba_mul(half_len, r1, 1, r3, 1, r3, half_len+1, buf, 1, buf2, 1)
     r1 .-= r2 .+ r0
 
     # TODO: assuming p.negacyclic here, adding a variant for -1 should be simple
