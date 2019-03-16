e7796295bf4f79aeeed722882c752dbbcb51c378
Bogdan Opanchuk <bogdan@opanchuk.net>
2019-03-16 23:59:20 +1100

    montgomery_reduction: use a different Montgomery coefficient, leading to less conditions and better vectorization

--- src/montgomery_reduction.jl
+++ src/montgomery_reduction.jl
@@ -1,5 +1,20 @@
 """
 Underlying algorithms for Montgomery representation of integers.
+
+The algorithm is slightly different to the classical one (found e.g. in Knuth):
+instead of `-m^(-1) mod b` as the coefficient, we have just `m^(-1) mod b`.
+Consequently, this leads to a plus replaced with minus at one stage during multiplication,
+and one less condition and addition for single-limb numbers.
+
+The idea is that during Montgomery reduction or multiplication,
+in the sum where the coefficient is used, the lowest limb gets turned to 0 during an addition
+(due to the choice of the coefficient).
+If `x+y mod b == 0`, `x+y` can be either `0` or `b`,
+so we have to check for carry and add it to the result.
+With our new coefficient, we have `x-y mod b == 0`, so there's no carry needed.
+
+This leads to 4x performance increase for arrays of UInt64, for example,
+because the new version can be vectorized much more efficiently.
 """
 
 using Base: setindex
@@ -7,12 +22,12 @@ using Base: setindex
 
 """
 Calculate the coefficient used for multiplication and conversion from M. representation.
-Namely, `m' = -m^(-1) mod b`, where `b = typemax(T)+1`.
+Namely, `m' = m^(-1) mod b`, where `b = typemax(T)+1`.
 """
 function get_montgomery_coeff(modulus::T) where T <: Unsigned
     # have to widen the type, since we need to use `typemax(T)+1`.
     T2 = widen(T)
-    -T(invmod(convert(T2, modulus), convert(T2, typemax(T)) + 1))
+    T(invmod(convert(T2, modulus), convert(T2, typemax(T)) + 1))
 end
 
 
@@ -28,47 +43,42 @@ end
 
 
 """
-Calculates `x + c * v`, where `x` is a multi-precision number of length `N+1`
-(passed as a number of length `N` + the highmost digit),
-`v` is a multi-precision number of length `N`, and `c` is a single radix digit.
-Returns a tuple of the result and the carry bit (if there's an overflow in the `N+1`-th digit).
+Calculates `x * y`, where `x` is a multi-precision number of length `N`,
+and `y` is a single radix digit.
+Returns the `N+1`-long result as tuple of the lowest digit
+and a multi-precision number with the `N` highest digits.
 """
-@inline @generated function _mul_addto_g(
-        x::MPNumber{N, T}, x_hi::T, c::T, v::MPNumber{N, T}) where {N, T}
+@inline @generated function _mul_by_single(x::MPNumber{N, T}, y::T) where {N, T}
 
-    # A generated version is a bit uglier, but works noticeably faster.
+    body = []
 
-    loop_body = []
     for j in 1:N
-        push!(loop_body, quote
-            new_hi, lo = mulhilo(c, v[$j])
+        push!(body, quote
+            hi, lo = mulhilo(x[$j], y)
+        end)
 
-            r, new_hi_carry1 = _addc(x[$j], lo)
-            r, new_hi_carry2 = _addc(r, hi)
-            r, new_hi_carry3 = _addc(r, T(hi_carry1 + hi_carry2 + hi_carry3))
+        if j > 1
+            push!(body, quote
+                hi, lo = addhilo(hi, lo, c)
+                hi, lo = addhilo(hi, lo, w[$(j-1)])
+            end)
+        end
 
-            out = setindex(out, r, $j)
-            hi = new_hi
-            hi_carry1 = new_hi_carry1
-            hi_carry2 = new_hi_carry2
-            hi_carry3 = new_hi_carry3
+        push!(body, quote
+            $(j == 1 ? :(w_lo = lo) : :(w = setindex(w, lo, $(j-1))))
+            c = hi
         end)
     end
 
     quote
-        hi_carry1 = false
-        hi_carry2 = false
-        hi_carry3 = false
-        hi = zero(T)
-        out = zero(MPNumber{N, T})
+        w = zero(MPNumber{N, T})
+        c = zero(T)
+        w_lo = zero(T)
 
-        $(loop_body...)
+        $(body...)
 
-        out_hi, c1 = _addc(x_hi, hi)
-        out_hi, c2 = _addc(out_hi, T(hi_carry1 + hi_carry2 + hi_carry3))
-
-        # TODO: it seems that we can have at most 1 carried over to the n+2-th digit
-        out, out_hi, c1 || c2
+        w = setindex(w, c, N)
+        w_lo, w
     end
 end
 
@@ -79,35 +89,64 @@ For `x = x' * R mod m` and `y = y' * R mod m`
 calculates `x' * y' * R mod m`, where `R = typemax(MPNumber{N, T}) + 1`.
 `m_prime` is the Montgomery coefficient (see [`get_montgomery_coeff`](@ref)).
 """
-@Base.propagate_inbounds @inline function mulmod_montgomery(
+@Base.propagate_inbounds @inline @generated function mulmod_montgomery(
         x::MPNumber{N, T}, y::MPNumber{N, T}, m::MPNumber{N, T}, m_prime::T) where {N, T}
 
-    a = zero(MPNumber{N, T})
-    a_hi = zero(T)
+    body = []
+
     for i in 1:N
+        push!(body, quote
+            p1_lo, p1 = _mul_by_single(y, x[$i])
+            t = p1_lo
+        end)
 
-        u = (a[1] + x[i] * y[1]) * m_prime
-
-        # A = A + x[i] * y + u * m
-        a, a_hi, c1 = _mul_addto_g(a, a_hi, x[i], y)
-        a, a_hi, c2 = _mul_addto_g(a, a_hi, u, m)
-
-        # A = A / b
-        for j in 1:N-1
-            a = setindex(a, a[j+1], j)
+        if i > 1
+            push!(body, quote
+                t += a_lo
+                a += p1
+            end)
+        else
+            push!(body, quote
+                a = p1
+            end)
         end
-        a = setindex(a, a_hi, N)
 
-        # TODO: is it actually ever greater than 0? It seems to always be in tests.
-        # Setting it to just zero improves the performance a lot.
-        a_hi = T(c1 + c2)
+        push!(body, quote
+            u = t * m_prime
+            p2_lo, p2 = _mul_by_single(m, u)
+        end)
+
+        # a_lo + p1_lo - p2_lo is always zero mod radix (sizeof(T)+1)
+        # We just want to know if we need to carry 1 or not to the higher limbs.
+        # If i == 1, a_lo = 0, so the carry is also 0.
+        if i > 1
+            push!(body, quote
+                if (a_lo + p1_lo) < a_lo
+                    a += one(MPNumber{N, T})
+                end
+            end)
+        end
+
+        push!(body, quote
+            # TODO: can `a` be greater than `m` at this point?
+            a = submod(a, p2, m)
+        end)
+
+        if i == N
+            push!(body, quote
+                a
+            end)
+        else
+            new_a = [:(a[$j]) for j in 2:N]
+            push!(body, quote
+                a_lo = a[1]
+                a = MPNumber{N, T}(($(new_a...), zero(T)))
+            end)
+        end
     end
 
-    # It can be proven that `a` is at most `2m - 1`.
-    if a_hi > 0 || a >= m
-        a - m
-    else
-        a
+    quote
+        $(body...)
     end
 end
 
