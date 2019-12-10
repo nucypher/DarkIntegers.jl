17cc91868f1d204d393950b5fa97a646eebfdcce
Bogdan Opanchuk <bogdan@opanchuk.net>
2019-12-09 18:15:54 -0800

    utils: hide `hastypemax()` in case it intersects with the Base one

--- src/utils.jl
+++ src/utils.jl
@@ -2,15 +2,15 @@
 # as a workaround for https://github.com/JuliaLang/julia/issues/29971
 # Replace by `invmod` when it is merged.
 
-hastypemax(::Base.BitIntegerType) = true
-hastypemax(::Type{T}) where {T} = applicable(typemax, T)
+_hastypemax(::Base.BitIntegerType) = true
+_hastypemax(::Type{T}) where T = applicable(typemax, T)
 
 function invmod_(n::T, m::T) where T <: Integer
     g, x, y = gcdx(n, m)
     g != 1 && throw(DomainError((n, m), "Greatest common divisor is $g."))
     m == 0 && throw(DomainError(m, "`m` must not be 0."))
     # Note that m might be negative here.
-    r = (T <: Unsigned && hastypemax(typeof(x)) && x > typemax(x)>>1) ? mod(x + m, m) : mod(x, m)
+    r = (T <: Unsigned && _hastypemax(typeof(x)) && x > typemax(x)>>1) ? mod(x + m, m) : mod(x, m)
     # The postcondition is: mod(r * n, m) == mod(T(1), m) && div(r, m) == 0
     r
 end
